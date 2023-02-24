69f9b13226bb1939f4658fe78fd0dc66e17f1743
nimblexz <109966757+nimblexz@users.noreply.github.com>
2023-02-24 06:22:58 +0400

    Updates

---
+++ precache-manifest.de96ce3f27920d651f47a1d05a63cebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf97cd0ef0e5e0b495661303f24d10d",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "b4857602de5819c8dc4a",
+    "url": "/todolist/static/css/main.c9953377.chunk.css"
+  },
+  {
+    "revision": "e6f885f070e49e25333e",
+    "url": "/todolist/static/js/2.6a2602bb.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist/static/js/2.6a2602bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4857602de5819c8dc4a",
+    "url": "/todolist/static/js/main.8b09626b.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
